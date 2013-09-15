f76e3b728036e41f6b33f9ad1d19e98b896525ca
Gabriel Tessarolli <gabriel.tessarolli@gmail.com>
2013-09-15 17:03:17 -0300

    adjusting gui

--- src/main/java/uff/dew/vphadoop/ui/VPGui.java
+++ src/main/java/uff/dew/vphadoop/ui/VPGui.java
@@ -11,11 +11,12 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.border.EtchedBorder;
 
 public class VPGui {
 
-    JFrame frame;
+    private JFrame frame;
     
     public VPGui() {
         
@@ -30,9 +31,105 @@ public class VPGui {
         
         pane.setLayout(new GridBagLayout());
 
+        addDBConfigArea(pane);
         addQueryArea(pane);
         addButtonArea(pane);
         addOutputArea(pane);
+    }
+
+    private void addDBConfigArea(Container pane) {
+
+        GridBagConstraints cl = new GridBagConstraints();
+        cl.fill = GridBagConstraints.NONE;
+        cl.weightx = 0.0; 
+        cl.weighty = 0.0;
+        cl.gridx = 0;
+        cl.gridy = 0;
+        cl.gridwidth = 8;
+        cl.insets = new Insets(10, 10, 0, 0);
+        cl.anchor = GridBagConstraints.WEST;
+        pane.add(new JLabel("DB Configuration"), cl);
+        
+        // host
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.NONE;
+        c.weightx = 0.0; 
+        c.weighty = 0.0;
+        c.gridx = 0;
+        c.gridy = 1;
+        c.insets = new Insets(10, 10, 0, 0);
+        c.anchor = GridBagConstraints.WEST;
+        pane.add(new JLabel("host"), c);
+        
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.weighty = 0.0;
+        c.gridx = 1;
+        c.gridy = 1;
+        c.insets = new Insets(10, 0, 0, 0);
+        JTextField hostField = new JTextField("127.0.0.1");
+        hostField.setHorizontalAlignment(JTextField.CENTER);
+        pane.add(hostField,c);
+        
+        // port
+        c.fill = GridBagConstraints.NONE;
+        c.weightx = 0.0; 
+        c.weighty = 0.0;
+        c.gridx = 2;
+        c.gridy = 1;
+        c.insets = new Insets(10, 10, 0, 0);
+        c.anchor = GridBagConstraints.WEST;
+        pane.add(new JLabel("port"), c);
+        
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.weighty = 0.0;
+        c.gridx = 3;
+        c.gridy = 1;
+        c.insets = new Insets(10, 0, 0, 0);
+        JTextField portField = new JTextField("1984");
+        portField.setHorizontalAlignment(JTextField.CENTER);
+        pane.add(portField,c);
+        
+        // username
+        c.fill = GridBagConstraints.NONE;
+        c.weightx = 0.0; 
+        c.weighty = 0.0;
+        c.gridx = 4;
+        c.gridy = 1;
+        c.insets = new Insets(10, 10, 0, 0);
+        c.anchor = GridBagConstraints.WEST;
+        pane.add(new JLabel("username"), c);
+        
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.weighty = 0.0;
+        c.gridx = 5;
+        c.gridy = 1;
+        c.insets = new Insets(10, 0, 0, 0);
+        JTextField usernameField = new JTextField("admin");
+        usernameField.setHorizontalAlignment(JTextField.CENTER);
+        pane.add(usernameField,c);
+        
+        // password
+        c.fill = GridBagConstraints.NONE;
+        c.weightx = 0.0; 
+        c.weighty = 0.0;
+        c.gridx = 6;
+        c.gridy = 1;
+        c.insets = new Insets(10, 10, 0, 0);
+        c.anchor = GridBagConstraints.WEST;
+        pane.add(new JLabel("password"), c);
+        
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.weighty = 0.0;
+        c.gridx = 7;
+        c.gridy = 1;
+        c.insets = new Insets(10, 0, 0, 10);
+        JTextField passwordField = new JTextField("admin");
+        passwordField.setHorizontalAlignment(JTextField.CENTER);
+        pane.add(passwordField,c);
     }
 
     private void addQueryArea(Container pane) {
@@ -42,19 +139,21 @@ public class VPGui {
         cl.weightx = 0.5; 
         cl.weighty = 0.0;
         cl.gridx = 0;
-        cl.gridy = 0;
+        cl.gridy = 2;
+        cl.gridwidth = 8;
         cl.insets = new Insets(10, 10, 0, 0);
         cl.anchor = GridBagConstraints.WEST;
-        pane.add(label, cl);    
+        pane.add(label, cl);
+        
         JTextArea queryArea = new JTextArea();
         queryArea.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
-
         GridBagConstraints c = new GridBagConstraints();
         c.fill = GridBagConstraints.BOTH;
         c.weightx = 0.5; 
         c.weighty = 0.5;
         c.gridx = 0;
-        c.gridy = 1;
+        c.gridy = 3;
+        c.gridwidth = 8;
         c.insets = new Insets(10, 10, 10, 10);
         pane.add(queryArea,c);
     }
@@ -67,7 +166,8 @@ public class VPGui {
         GridBagConstraints c = new GridBagConstraints();
         c.weighty = 0;
         c.gridx = 0;
-        c.gridy = 2;
+        c.gridy = 4;
+        c.gridwidth = 8;
         c.anchor = GridBagConstraints.CENTER;
         pane.add(queryButton, c);
     }
@@ -79,7 +179,8 @@ public class VPGui {
         cl.weightx = 0.5; 
         cl.weighty = 0.0;
         cl.gridx = 0;
-        cl.gridy = 3;
+        cl.gridy = 5;
+        cl.gridwidth = 8;
         cl.insets = new Insets(10, 10, 0, 0);
         cl.anchor = GridBagConstraints.WEST;
         pane.add(label, cl);        
@@ -92,7 +193,8 @@ public class VPGui {
         c.weightx = 0.5; 
         c.weighty = 0.5;
         c.gridx = 0;
-        c.gridy = 4;
+        c.gridy = 6;
+        c.gridwidth = 8;
         c.insets = new Insets(10, 10, 10, 10);
         pane.add(outputArea, c);
     }
